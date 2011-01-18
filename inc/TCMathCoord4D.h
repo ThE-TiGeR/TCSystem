b0115228349aa234459ed5d8af4b7cddf0742eeb
Thomas <Thomas@localhost>
2011-01-18 23:45:37 +0100

    id changes

--- inc/TCMathCoord4D.h
+++ inc/TCMathCoord4D.h
@@ -30,7 +30,7 @@
 // License along with this library; if not, write to the Free Software       
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 //*******************************************************************************
-//  $Id: TCMathCoord4D.h 884 2008-07-24 09:48:48Z the_____tiger $
+//  $Id$
 //*******************************************************************************
 
 #ifndef _TC_MATH_COORD4D_H_
