194a3e65f369dace3280596f136d7b171346250a
Thomas Goessler <thomas@the-goesslers.com>
2013-09-30 23:59:49 +0200

    changed to build_win64 folder enable of find boost per default

---
+++ Tests/RegexTest/TCRegexTest.cpp
@@ -0,0 +1,12 @@
+#include "TCOutput.h"
+
+#include <boost/regex.hpp>
+
+#include "TCDebugNew.h"
+
+int main(int, char**)
+{
+   int status = 0;
+   return status;
+}
+
--- create_win64.bat
+++ create_win64.bat
@@ -1,5 +1,5 @@
-mkdir build_win32
+mkdir build_win64
 set INSTALL_DIR=%~dp0\..\installed
-cd build_win32
+cd build_win64
 cmake -G "Visual Studio 11 Win64" -DBUILD_SHARED_LIBS=true -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..
 pause
